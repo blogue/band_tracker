2ab3032139fa255aa226a7143db806cfa0d928e3
Ben Logue <benjamin.logue73@gmail.com>
2016-07-22 10:17:24 -0700

    add views for list venues, add venue and view instance of venue.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,25 @@ namespace BandTracker.Objects
     public HomeModule()
     {
       Get["/"] = _ => View["index.cshtml"];
+
+      Get["/venues"] = _ =>
+      {
+        List<Venue> allVenues = Venue.GetAll();
+        return View["venues.cshtml", allVenues];
+      };
+      Get["/venues/add"] = _ => View["venue_new.cshtml"];
+      Post["/venues"] = _ =>
+      {
+        Venue newVenue = new Venue(Request.Form["name"]);
+        newVenue.Save();
+        List<Venue> allVenues = Venue.GetAll();
+        return View["venues.cshtml", allVenues];
+      };
+      Get["/venues/{id}"] = parameters =>
+      {
+        Venue selectedVenue = Venue.Find(parameters.id);
+        return View["venue.cshtml", selectedVenue];
+      };
     }
   }
 
